4668c3687e2efde61cf5f448aaaae4a16d9e2acb
Michael Tovmach <michaeltovmach.noreply@gmail.com>
2022-03-16 16:55:32 +0100

    feat(FavoritePage): loading and no favorite added

--- pages/favorites/index.js
+++ pages/favorites/index.js
@@ -3,30 +3,49 @@ import PropertyCardList from '../../components/Card/PropertyCardList'
 import { useEffect } from 'react'
 import { useState } from 'react'
 import axios from 'axios'
+import Typography from '@mui/material/Typography'
+import Container from '@mui/material/Container'
+import CircularProgress from '@mui/material/CircularProgress'
+import { Box } from '@mui/material'
 
 const FavoritesPage = () => {
   const [favoritesData, setFavoritesData] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const listOfIds = JSON.parse(localStorage.getItem('favorites'))
 
     if (listOfIds === null) {
       localStorage.setItem('favorites', JSON.stringify([]))
+      setLoading(false)
     }
     if (listOfIds.length !== 0) {
       axios
         .post('/api/properties-from-id-list', {
           listOfIds,
         })
-        .then((result) => setFavoritesData(result.data))
+        .then((result) => {
+          setFavoritesData(result.data)
+          setLoading(false)
+        })
+    } else {
+      setLoading(false)
     }
   }, [])
-  console.log(favoritesData)
 
   return (
     <>
-      {favoritesData.length === 0 ? (
-        <p>no favorites</p>
+      <Container maxWidth='lg'>
+        <Typography variant='h2' color='secondary' sx={{ fontSize: '2rem' }}>
+          Favorites
+        </Typography>
+        <Typography variant='h1' color='primary' sx={{ fontSize: '2.75rem' }}>
+          Properties you liked
+        </Typography>
+      </Container>
+      {loading && <CircularProgress color='secondary' sx={{ mx: 'auto' }} />}
+      {!loading && favoritesData.length === 0 ? (
+        <Box sx={{ mx: 'auto', mt: 2 }}>No Favorites</Box>
       ) : (
         <PropertyCardList data={favoritesData} />
       )}
