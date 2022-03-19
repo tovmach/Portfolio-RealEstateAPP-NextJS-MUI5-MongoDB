762285b00a028b1650ce0d6a8556aca8a303c5b9
Michael Tovmach <michaeltovmach.noreply@gmail.com>
2022-03-19 16:05:16 +0100

    feat(api/contact): saves data to google spreadSheet

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,14 +1,44 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import axios from 'axios'
+import { google } from 'googleapis'
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { name, email, phone, message } = req.body
 
-    const tmMessage = `<b>Name:</b> ${name} 
-<b>Phone:</b> ${phone} 
-<b>Email:</b> ${email} 
+    let d = new Date()
+    let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d)
+    let mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(d)
+    let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d)
+
+    let time = d.getHours() + ':' + d.getMinutes()
+
+    const todayDate = `${da}-${mo}-${ye} ${time}`
+
+    const target = ['https://www.googleapis.com/auth/spreadsheets']
+    const jwt = new google.auth.JWT(
+      process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+      null,
+      (process.env.GOOGLE_SHEETS_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
+      target
+    )
+
+    const sheets = google.sheets({ version: 'v4', auth: jwt })
+
+    await sheets.spreadsheets.values.append({
+      spreadsheetId: process.env.SPREADSHEET_ID,
+      range: 'Data', // sheet name
+      valueInputOption: 'USER_ENTERED',
+      resource: {
+        values: [[todayDate, name, email, phone, message]],
+      },
+    })
+
+    const tmMessage = `<b>Date:</b> ${todayDate}
+<b>Name:</b> ${name}
+<b>Phone:</b> ${phone}
+<b>Email:</b> ${email}
 <b>Message:</b> ${message}`
 
     const urlEncodedMessage = encodeURIComponent(tmMessage)
