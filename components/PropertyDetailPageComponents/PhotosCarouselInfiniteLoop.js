e880e784028722e95ada53d5295c4f30e7d3066d
Michael Tovmach <michaeltovmach.noreply@gmail.com>
2022-03-29 17:19:25 +0200

    feat: photos carousel added to PropertyDetailPage

---
+++ components/PropertyDetailPageComponents/PhotosCarouselInfiniteLoop.js
@@ -0,0 +1,75 @@
+import React, { useRef, useState } from 'react'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+
+// import required modules
+import { Pagination, Navigation } from 'swiper'
+import { Box } from '@mui/system'
+import Image from 'next/image'
+
+const imgList = [
+  '/demo-house/demo-house-0.jpg',
+  '/demo-house/demo-house-1.jpg',
+  '/demo-house/demo-house-2.jpg',
+  '/demo-house/demo-house-3.jpg',
+  '/demo-house/demo-house-4.jpg',
+  '/demo-house/demo-house-5.jpg',
+  '/demo-house/demo-house-6.jpg',
+]
+
+export default function PhotosCarouselInfiniteLoop() {
+  return (
+    <>
+      <Box
+        sx={{
+          maxWidth: 1152,
+          mx: 'auto',
+          '.swiper': {
+            width: '100%',
+            height: 500,
+            borderRadius: 2,
+          },
+          '.swiper-button-next': {
+            color: 'white',
+          },
+          '.swiper-button-prev': {
+            color: 'white',
+          },
+          '.swiper-pagination-bullet': {
+            bgcolor: 'white',
+          },
+        }}
+      >
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          loop={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          className='mySwiper'
+        >
+          {imgList.map((img) => (
+            <SwiperSlide key={img}>
+              <Box sx={{ width: '100%', height: '100%' }}>
+                <Image
+                  src={img}
+                  layout='fill'
+                  objectFit='cover'
+                  alt={` photo`}
+                />
+              </Box>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Box>
+    </>
+  )
+}
--- pages/properties/[id].js
+++ pages/properties/[id].js
@@ -1,18 +1,56 @@
 import React from 'react'
 import PhotosCarousel from '../../components/PropertyDetailPageComponents/PhotosCarousel'
-import { Box } from '@mui/material'
+import { Box, Tooltip, Typography } from '@mui/material'
 import ContactForm from '../../components/ContactForm'
 import mongoose from 'mongoose'
 import Property from '../../models/propertyModel'
+import { Paper, Container } from '@mui/material'
+import PhotosCarouselInfiniteLoop from '../../components/PropertyDetailPageComponents/PhotosCarouselInfiniteLoop'
+
+const capitalizeFirstLetter = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+const numberWithCommas = (num) => {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+}
 
 const PropertyDetailPage = ({ data }) => {
+  const item = data[0]
   return (
     <>
-      <Box mt={2}>
+      {/* <Box mt={2}>
         <PhotosCarousel />
+      </Box> */}
+
+      <Box mt={2}>
+        <PhotosCarouselInfiniteLoop />
+      </Box>
+
+      <Box mt={2}>
+        <Typography
+          sx={{ fontSize: '2.5rem', color: 'primary.main', fontWeight: 'bold' }}
+          textAlign={'center'}
+        >
+          {capitalizeFirstLetter(item.type)} in{' '}
+          {capitalizeFirstLetter(item.city)} for{' '}
+          {item.operation === 'buy' ? 'Sale' : 'Rent'}
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: '1.5rem',
+            color: 'secondary.main',
+            fontWeight: 'bold',
+          }}
+          variant='body1'
+          color='initial'
+          textAlign={'center'}
+        >
+          {numberWithCommas(item.price)} €
+        </Typography>
       </Box>
       <Box mt={4}>
-        <ContactForm data={data[0]} />
+        <ContactForm data={item} />
       </Box>
     </>
   )
@@ -38,11 +76,10 @@ export const getServerSideProps = async (ctx) => {
   }).lean()
 
   data = JSON.parse(JSON.stringify(data))
-  console.log(data)
+
   return {
     props: {
       data,
-      query,
     },
   }
 }
