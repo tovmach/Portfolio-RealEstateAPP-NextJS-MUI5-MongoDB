cfc170f59c72204d13c4bb1e5da2fe4e550ead5f
Michael Tovmach <michaeltovmach.noreply@gmail.com>
2022-04-01 11:00:33 +0200

    feat(PropertyEditPage): updates property information

--- pages/admin/[id].js
+++ pages/admin/[id].js
@@ -3,14 +3,110 @@ import mongoose from 'mongoose'
 import Property from '../../models/propertyModel'
 import { Container } from '@mui/material'
 import Grid from '@mui/material/Grid'
-import { red } from '@mui/material/colors'
 import DeletePropertyButton from '../../components/adminPageComponents/DeletePropertyButton'
+import PropertyForm from '../../components/adminPageComponents/PropertyForm'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 const PropertyEditPage = ({ data }) => {
+  const router = useRouter()
+
+  const [operation, setOperation] = useState('')
+  const [type, setType] = useState('')
+  const [price, setPrice] = useState('')
+  const [city, setCity] = useState('')
+  const [livingArea, setLivingArea] = useState('')
+  const [plot, setPlot] = useState('')
+  const [bedroom, setBedroom] = useState('')
+  const [bathroom, setBathroom] = useState('')
+  const [description, setDescription] = useState('')
+
+  useEffect(() => {
+    if (data.operation) {
+      setOperation(data.operation)
+    }
+    if (data.type) {
+      setType(data.type)
+    }
+    if (data.price) {
+      setPrice(data.price)
+    }
+    if (data.city) {
+      setCity(data.city)
+    }
+    if (data.livingArea) {
+      setLivingArea(data.livingArea)
+    }
+    if (data.plot) {
+      setPlot(data.plot)
+    }
+    if (data.bedroom) {
+      setBedroom(data.bedroom)
+    }
+    if (data.bathroom) {
+      setBathroom(data.bathroom)
+    }
+    if (data.description) {
+      setDescription(data.description)
+    }
+  }, [])
+
+  const onSubmitHandler = () => {
+    axios
+      .post('/api/update-property', {
+        id: data._id,
+        operation,
+        type,
+        price,
+        city,
+        livingArea,
+        plot,
+        bedroom,
+        bathroom,
+        description,
+        img: '/villa.jpg',
+      })
+      .then((response) => {
+        setOperation('')
+        setType('')
+        setPrice('')
+        setCity('')
+        setLivingArea('')
+        setPlot('')
+        setBedroom('')
+        setBathroom('')
+        setDescription('')
+        router.push('/admin')
+      })
+  }
+
   return (
     <>
+      <PropertyForm
+        operation={operation}
+        setOperation={setOperation}
+        type={type}
+        setType={setType}
+        price={price}
+        setPrice={setPrice}
+        city={city}
+        setCity={setCity}
+        livingArea={livingArea}
+        setLivingArea={setLivingArea}
+        plot={plot}
+        setPlot={setPlot}
+        bedroom={bedroom}
+        setBedroom={setBedroom}
+        bathroom={bathroom}
+        setBathroom={setBathroom}
+        description={description}
+        setDescription={setDescription}
+        onSubmitHandler={onSubmitHandler}
+      />
       <Container maxWidth='sm'>
-        <Grid container spacing={2} alignItems={'center'}>
+        <Grid container alignItems={'center'} mt={2}>
           <Grid item xs>
             <DeletePropertyButton id={data._id} />
           </Grid>
