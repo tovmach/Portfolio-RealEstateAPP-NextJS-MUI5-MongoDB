a7e9c99cf2acbd5405b2d8b2647f0ce468e1bea6
Michael Tovmach <michaeltovmach.noreply@gmail.com>
2022-04-07 14:44:41 +0200

    fix(CreatePropertyForm): button disable when no plot fixed

--- components/adminPageComponents/CreatePropertyForm.js
+++ components/adminPageComponents/CreatePropertyForm.js
@@ -230,7 +230,6 @@ const CreatePropertyForm = ({
                 price.length === 0 ||
                 city.length === 0 ||
                 livingArea.length === 0 ||
-                plot.length === 0 ||
                 bedroom.length === 0 ||
                 bathroom.length === 0 ||
                 description.length === 0
